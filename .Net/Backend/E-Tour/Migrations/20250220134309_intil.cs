f6ba1acb21febd279ec481e234c6e572eeb48490
ssanket0987 <me.sanket.shinde@gmail.com>
2025-02-21 14:37:46 +0530

    Initial project upload

---
+++ .Net/Backend/E-Tour/Migrations/20250220134309_intil.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_Tour.Migrations
+{
+    /// <inheritdoc />
+    public partial class intil : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "price",
+                table: "tours");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "singlePersonCost",
+                table: "costmaster",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "price",
+                table: "tours",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "singlePersonCost",
+                table: "costmaster",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
